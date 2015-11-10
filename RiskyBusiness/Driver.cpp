7f184784171d017a3e5adfbef97ba3b3728f5fe7
Nejteh <marc.kaloussian@gmail.com>
2015-11-10 17:59:19 -0500

    derp

--- RiskyBusiness/Driver.cpp
+++ RiskyBusiness/Driver.cpp
@@ -4,7 +4,6 @@
 int main() {
 	srand(time(0)); //used to randomize number when rand() is used
 
-<<<<<<< HEAD
 	//ask user if they want to edit/make map
 	MapController discard; 
 	
@@ -12,10 +11,7 @@ int main() {
 	//Start game
 	//choose map
 
-	GameController GC;
-=======
 	GameController* GC = new GameController();
->>>>>>> Print map only for current player.
 
 	MapView* mapView = new MapView(GC);
 	mapView->displayBoard();
