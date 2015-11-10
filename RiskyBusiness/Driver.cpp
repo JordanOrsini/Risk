14e7b92fb41c48ba3d889c3f53931360570766b8
Melle Buruma <milesburuma@gmail.com>
2015-11-10 18:35:01 -0500

    Changed driver

--- RiskyBusiness/Driver.cpp
+++ RiskyBusiness/Driver.cpp
@@ -4,17 +4,13 @@
 int main() {
 	srand(time(0)); //used to randomize number when rand() is used
 
-	//ask user if they want to edit/make map
-	MapController discard; 
-	
-	
-	//Start game
-	//choose map
+	// Pregame map creatio n
+	MapView tempView(); 
 
+	// Start actual game
 	GameController* GC = new GameController();
 
 	MapView* mapView = new MapView(GC);
-	mapView->displayBoard();
 
 	GC->startUpPhase();
 
